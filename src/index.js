755cbc85a165ea40b9b488ece1e21cc9022646dc
Christian Lasso <classob1108@outlook.com>
2022-11-28 16:29:22 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// Settings
+
+
+// Middlewares
+
+
+// Routes
+
+
+// Static files
+
+
+// Startinf the server
+app.listen(3000,()=>{
+    console.log('server on port 3000')
+})
